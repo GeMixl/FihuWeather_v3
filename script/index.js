98cda7ac32af99a06e34288c5ed1c1b477e47d65
GeMixl <g.michl@gmx.net>
2021-05-20 14:27:05 +0200

    Finished set-up of the project structure. Frontend works statically.

--- script/index.js
+++ script/index.js
@@ -1,5 +1,6 @@
 import { loadAndProcessData } from './loadAndProcessData.js'
 import { plotTemperatureChart } from './plotTemperatureChart.js'
+import { plotHumidityChart } from './plotHumidityChart.js'
 
 const margin = ({top: 20, right: 30, bottom: 30, left: 40});
 const height = document.querySelector('#temperature-chart').offsetWidth * 2 / 3;
@@ -7,6 +8,11 @@ const width = document.querySelector('#temperature-chart').offsetWidth;
 
 const render = (data) => {
   plotTemperatureChart(d3.select('#temperature-chart'), {data: data,
+    width: width,
+    height: height,
+    margin: margin}
+  );
+  plotHumidityChart(d3.select('#humidity-chart'), {data: data,
     width: width,
     height: height,
     margin: margin}
---
+++ script/plotHumidityChart.js
@@ -0,0 +1,47 @@
+export const plotHumidityChart = (selection, props) => {
+  const {
+    data,
+    width,
+    height,
+    margin
+  } = props;
+
+  const yAxis = g => g
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y))
+
+  const xAxis = g => g
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
+
+  const y = d3.scaleLinear()
+    .domain([d3.min(data, d => d.humidity), d3.max(data, d => d.humidity)]).nice()
+      .range([height - margin.bottom, margin.top])
+
+  const x = d3.scaleTime()
+    .domain(d3.extent(data, d => d.timestamp))
+      .range([margin.left, width - margin.right])
+
+  const line = d3.line()
+      .x(d => x(d.timestamp))
+      .y(d => y(d.humidity))
+
+  const svg = selection
+    .append("svg")
+      .attr("viewBox", [0, 0, width, height]);
+
+  svg.append("g")
+      .call(xAxis);
+
+  svg.append("g")
+      .call(yAxis);
+
+  svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("d", line);
+}
