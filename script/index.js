b4ccd55031806537a760e6aaf31842013e39af05
GeMixl <g.michl@gmx.net>
2021-06-03 23:16:56 +0200

    Temperature pane works now except for a small bug with transitions.

--- script/index.js
+++ script/index.js
@@ -6,17 +6,47 @@ const margin = ({top: 20, right: 30, bottom: 30, left: 40});
 const height = document.querySelector('#temperature-chart').offsetWidth * 2 / 3;
 const width = document.querySelector('#temperature-chart').offsetWidth;
 
-const render = (data) => {
-  plotTemperatureChart(d3.select('#temperature-chart'), {data: data,
+let data;
+
+let status = {
+  xStart: null
+};
+
+const xRangeSelector = document.querySelector('#x-range-selector');
+//console.log(xRangeSelector);
+xRangeSelector.addEventListener('change', (event) => {
+  switch (xRangeSelector.selectedIndex) {
+    case 0:
+      status.xStart = 7;
+      break;
+    case 1:
+      status.xStart = 14;
+      break;
+    case 2:
+      status.xStart = 30;
+      break;
+    default:
+      status.xStart = null;
+  };
+  render();
+}, false);
+
+const render = () => {
+  console.log(data);
+  plotTemperatureChart(d3.selectAll('#temperature-chart'), {data: data,
+    status: status,
     width: width,
     height: height,
     margin: margin}
   );
-  plotHumidityChart(d3.select('#humidity-chart'), {data: data,
+  plotHumidityChart(d3.selectAll('#humidity-chart'), {data: data,
+    status: status,
     width: width,
     height: height,
     margin: margin}
   );
 };
 
-loadAndProcessData().then(data => render(data));
+loadAndProcessData()
+  .then(d => { data = d })
+  .then( () => { render() });
--- script/plotTemperatureChart.js
+++ script/plotTemperatureChart.js
@@ -1,10 +1,26 @@
+
 export const plotTemperatureChart = (selection, props) => {
   const {
     data,
+    status,
     width,
     height,
     margin
   } = props;
+
+   const setXExtent = function (xExtent, offsetDay) {
+    let xExtentStart = new Date(xExtent[0]);
+    let xExtentStop  = new Date(xExtent[1]);
+    if (offsetDay !== null) {
+      xExtentStart.setDate(xExtentStop.getDate() - offsetDay);
+    }
+    return [xExtentStart, xExtentStop];
+  };
+
+  console.log(d3.extent(data, d => d.timestamp))
+  console.log(setXExtent(d3.extent(data, d => d.timestamp), status.xStart))
+  console.log(d3.extent(data, d => d.timestamp))
+
 
   const yAxis = g => g
       .attr("transform", `translate(${margin.left},0)`)
@@ -19,25 +35,30 @@ export const plotTemperatureChart = (selection, props) => {
       .range([height - margin.bottom, margin.top])
 
   const x = d3.scaleTime()
-    .domain(d3.extent(data, d => d.timestamp))
+    .domain(setXExtent(d3.extent(data, d => d.timestamp), status.xStart))
       .range([margin.left, width - margin.right])
 
   const line = d3.line()
       .x(d => x(d.timestamp))
       .y(d => y(d.temperature))
 
-  const svg = selection
-    .append("svg")
-      .attr("viewBox", [0, 0, width, height]);
+  let svg = selection.selectAll("svg").data([null])
+    .join("svg")
+      .attr("viewBox", [0, 0, width, height])
 
-  svg.append("g")
-      .call(xAxis);
-
-  svg.append("g")
+  svg.selectAll("#y-axis").data([null])
+    .join(enter => enter.append("g").attr("id", "y-axis"))
       .call(yAxis);
 
-  svg.append("path")
-      .datum(data)
+  svg.selectAll("#x-axis").data([null])
+    .join(enter => enter.append("g").attr("id", "x-axis"))
+      .transition().duration(1000)
+      .call(xAxis);
+
+  svg.selectAll("#temp-line")
+      .data([data])
+      .join(enter => enter.append("path").attr("id", "temp-line"))
+      .transition().duration(1000)
       .attr("fill", "none")
       .attr("stroke", "steelblue")
       .attr("stroke-width", 1.5)
